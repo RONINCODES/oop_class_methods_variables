63ef01f3ae56c1a1d17f142f60966394eac95669
Ronen Annason <ronenannason83@gmail.com>
2017-05-25 21:36:50 -0400

    added drink blood method

--- vampires.rb
+++ vampires.rb
@@ -15,6 +15,12 @@ class Vampire
     new_vampire = Vampire.new(name, age)
     # add new vampires to list of vampires
     @@coven << new_vampire
+    new_vampire
   end
+
+  def drink_blood
+    @drank_blood_today = true
+  end
+
 
 end
