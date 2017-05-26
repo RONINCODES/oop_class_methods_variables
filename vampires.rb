bcfc10cf34888753ea6916c59bf5288879f01bb9
Ronen Annason <ronenannason83@gmail.com>
2017-05-25 21:19:17 -0400

    Vampire class created, initialize method

--- vampires.rb
+++ vampires.rb
@@ -1,39 +1,16 @@
 class Vampire
 
-def initialize(name, age)
-  @name = name
-  @age = age
+  @@coven =[]
 
-  @in_coffin? = false
-  @drank_blood_today? = false
-  @coven =[]
-
-  def self.create
-    new_vampire = Vampire.new(name, age)
-    @coven << new_vampire
-  end
-
-  def drank_blood_today
-    @drank_blood_today == true
-  end
-
-  def self.sunrise
-    if self.drank_blood_today == false || self.@in_coffin == true
-      self.@coven.delete
-    end
-  end
-
-  def self.sunset
-    @drank_blood_today = false
+  def initialize(name, age)
+    #set instance variables in initialize method (name,age)
+    @name = name
+    @age = age
+    # add instance variables in coffin, and drank blood today?
     @in_coffin = false
+    @drank_blood_today = false
+
+    # add new vampires to list of vampires
+    @@coven << self
   end
-   
-
-
-
-  def go_home
-    @in_coffin == true
-  end
-
-
 end
